355309383bc06261cef7aa8c6238192393bc0379
zuogerenba <1732935973@qq.com>
2020-04-16 12:46:56 +0800

    执行上下文

--- 执行上下文/index.js
+++ 执行上下文/index.js
@@ -1,10 +1,20 @@
-let obj = {
-  foo() {
-    console.log(this)
-  }
+// let obj = {
+//   foo() {
+//     console.log(this)
+//   }
+// }
+// obj.foo()
+
+// let bar = obj.foo
+
+// bar.apply(obj)
+
+function foo() {
+  console.log('foo1')
 }
-obj.foo()
+foo()
 
-let bar = obj.foo
-
-bar.apply(obj)
+function foo() {
+  console.log('foo2')
+}
+foo()
