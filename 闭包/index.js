ded4214f5b5de1553342e69666f4dc145576f015
zuogerenba <1732935973@qq.com>
2020-05-21 23:38:44 +0800

    闭包

--- 闭包/index.js
+++ 闭包/index.js
@@ -1,15 +1,15 @@
 // 面试题1 编写一个函数，该函数将遍历整数列表，并在延迟3s后打印每个元素的索引
 
 // 方法一 let
-// function fun() {
-//   let arr = [11,22,33,44,55]
-//   for(let i=0;i<arr.length;i++) {
-//     setTimeout(() => {
-//       console.log('index:',i)
-//     }, 3000);
-//   }
-// }
-// fun()
+function fun() {
+  let arr = [11,22,33,44,55]
+  for(let i=0;i<arr.length;i++) {
+    setTimeout(() => {
+      console.log('index:',i)
+    }, 3000);
+  }
+}
+fun()
 
 // 方法二 闭包
 const arr = [11, 22, 33, 44, 55]
