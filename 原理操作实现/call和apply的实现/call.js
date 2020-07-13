c52f60122ac7d0a7331dc47d6860c8952ee861af
zuogerenba <1732935973@qq.com>
2020-07-13 22:52:14 +0800

    js

---
+++ 原理操作实现/call和apply的实现/call.js
@@ -0,0 +1,21 @@
+Function.prototype.mycall = function(context){
+  context = context || window
+  context.fn = this;
+  let args = [...arguments].slice(1)
+  let result = context.fn(...args)
+  delete context.fn
+  return result
+}
+
+let obj = {
+  name: 'qmj',
+  age:20
+}
+var name = 'qqq'
+var foo = function(name,age) {
+  console.log(this.age)
+  console.log(this.name)
+  console.log(name)
+  console.log(age)
+}
+foo.mycall(obj,'qqq',18)
