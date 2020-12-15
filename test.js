dee4305dcf8342cd1b371bbad3b41b76e32396c5
zuogerenba <1732935973@qq.com>
2020-12-15 23:18:39 +0800

    [js学习]字符串截取

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ function foo(arr) {
   return  [...new Set(arr.toString().split(',').map(item => item*1).sort((a,b) => a -b))]
 }
 
-let h = 'head'
-console.log(h.toLowerCase().charAt(1),h)
+// #000000 -- #ffffff
+console.log(typeof Math.floor(Math.random()*0xffffff).toString(16))
